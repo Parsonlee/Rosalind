d238a385aea163f1c9b73d6aa633073ffad39247
ZHao <695700339@qq.com>
2021-08-19 23:29:57 +0800

    Finding a Protein Motif

--- findingMotif2.py
+++ findingMotif2.py
@@ -1,37 +1,42 @@
-from __future__ import print_function
 import re # for regex functions
+import urllib3
+from Bio import SeqIO
 
 # need regex for N-glycosylation motif
 # N{any but P}[S or T]{any but P}
 # parentheses and '?=' allow for overlapping
 motif = re.compile('(?=N[^P][ST][^P])')
-
-# loop through protein id's to import from uniprot website
-import urllib3
-from Bio import SeqIO
-
-dataset = open("rosalind_mprt.txt", 'r')
-protein_ids = dataset.readlines()
-dataset.close()
-
-outhandle = open("016_MPRT.txt", 'w')
 uniprot_url = "http://www.uniprot.org/uniprot/"
 http = urllib3.PoolManager()
-url = uniprot_url + 'B5ZC00' + ".fasta"
-f = http.request('GET', url)
-raw_data = SeqIO.read(f.data, 'fasta')
-print(raw_data)
-# for protein in protein_ids:
-#     url = uniprot_url+protein.rstrip()+".fasta"
-#     f = http.request('GET', url)
-#     raw_data = SeqIO.read(f.data, 'fasta')
 
-#     locations = []
-#     # Use search instead of match to search entire string
-#     if re.search(motif, str(raw_data.seq)):
-#         print(protein.strip(), file=outhandle)
-#         for m in re.finditer(motif, str(raw_data.seq)):
-#             locations.append(m.start()+1)
-#         print(" ".join(map(str, locations)), file=outhandle)
-        
-# outhandle.close()
+with open('rosalind_mprt.txt', 'r') as dataset:
+  protein_ids = dataset.readlines()
+  ids = [id.strip() for id in protein_ids]
+
+# create a file stroes pure sequences
+fas = open('fastas.txt', 'w')
+outhandle = open('016_MPRT.txt', 'w')
+
+# loop through protein id's to import from uniprot website
+for id in protein_ids:
+  url = uniprot_url + id.rstrip() + '.fasta'
+  f = http.request('GET', url)
+  # print(f.data.decode())
+  fas.write(f.data.decode())
+fas.close()
+
+seqs = {}
+for i, record in enumerate(SeqIO.parse('fastas.txt', 'fasta')):
+  # seqs.append(str(record.seq))
+  seqs[ids[i]] = str(record.seq)
+
+
+for id, seq in seqs.items():
+  locations = []
+  # Use search instead of match to search entire string
+  if re.search(motif, seq):
+    print(id, file=outhandle)
+    for m in re.finditer(motif, seq):
+        locations.append(m.start()+1)
+    print(" ".join(map(str, locations)), file=outhandle)
+outhandle.close()
